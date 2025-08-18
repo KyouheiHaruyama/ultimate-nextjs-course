1829ef7e81914496400200e30be8a10fab46efd3
春山享平 <haruyamakyouhei@haruyamakyouheinoMacBook-Pro.local>
2025-08-18 15:16:44 +0900

    Set up Light&Dark Theme, Shadcn, Navbar

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends(
       "next/core-web-vitals",
       "next/typescript",
-      "standard",
+      // "standard",
       // "plugin:tailwindcss/recommended",
       "prettier",
   ),
