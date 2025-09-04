74d2a9227938c7352ca17fe190e5a49089563460
春山享平 <haruyamakyouhei@haruyamakyouheinoMacBook-Pro.local>
2025-09-04 16:15:50 +0900

    Change Leftsidebar & Implement RightSidebar

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,4 +2,6 @@ export const ROUTES = {
     HOME: "/",
     SIGN_IN: "/sign-in",
     SIGN_UP: "/sign-up",
+    PROFILE: (_id: string) => `/profile/${_id}`,
+    TAGS: (_id: string) => `/tags/${_id}`,
 };
