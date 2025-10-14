5395e939f01f667f95366a1728ae65131b7f6adc
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-10-14 13:32:09 +0900

    Implement Question Form

--- lib/validations.ts
+++ lib/validations.ts
@@ -45,3 +45,23 @@ export const SignUpSchema = z.object({
             message: "Password must contain at least one special character.",
         }),
 });
+
+export const AskQuestionSchema = z.object({
+    title: z
+        .string()
+        .min(5, { message: "Title is required." })
+        .max(100, { message: "Title cannot exceed 100 characters." }),
+    content: z
+        .string()
+        .min(1, { message: "Description is required." }),
+    tags: z.array(
+        z.string()
+            .min(1, { message: "Tag is required." })
+            .max(30, { message: "Tag cannot exceed 30 characters." })
+            .regex(/^[a-zA-Z\s]+$/, {
+                message: "Tag can only contain letters and spaces.",
+            })
+    )
+    .min(1, { message: "At least one tag is required." })
+    .max(3, { message: "You can only add up to 3 tags." })
+})
