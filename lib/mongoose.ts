b214b142b430aef6a18e6f6774fc45813a749080
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-10-20 17:49:48 +0900

    Implements Logging

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,4 +1,5 @@
 import mongoose, { Mongoose } from 'mongoose';
+import logger from "@/lib/logger";
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined');
@@ -17,16 +18,19 @@ let cached = global.mongoose;
 if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
 const dbConnect = async (): Promise<Mongoose> => {
-    if (cached.conn) return cached.conn;
+    if (cached.conn) {
+        logger.info("Using existing database connection");
+        return cached.conn;
+    }
 
     if (!cached.promise) {
         cached.promise = mongoose.connect(MONGODB_URI, {
             dbName: 'DevFlow'
         }).then((result) => {
-            console.log("Connected to MongoDB");
+            logger.info("Connected to MongoDB");
             return result;
         }).catch((error) => {
-            console.error("Error connecting to MongoDB", error);
+            logger.error("Error connecting to MongoDB", error);
             throw error;
         });
     }
