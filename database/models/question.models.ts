8d2981aec1e4f41388aca2f47ce9b74fc669869b
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-10-20 12:30:12 +0900

    Implement Database Model

---
+++ database/models/question.models.ts
@@ -0,0 +1,33 @@
+import {model, models, Schema, Types} from "mongoose";
+
+export interface IQuestion {
+    title: string;
+    content: string;
+    tags: Types.ObjectId[];
+    upvotes: number;
+    downvotes: number;
+    answers: number;
+    views: number;
+    author: Schema.Types.ObjectId;
+}
+
+const QuestionSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        content: { type: String, required: true },
+        tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
+
+        upvotes: { type: Number, default: 0 },
+        downvotes: { type: Number, default: 0 },
+        answers: { type: Number, default: 0 },
+        views: { type: Number, default: 0 },
+
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    }, {
+        timestamps: true
+    }
+);
+
+const Question = models?.Question || model<IQuestion>('Question', QuestionSchema);
+
+export default Question;
