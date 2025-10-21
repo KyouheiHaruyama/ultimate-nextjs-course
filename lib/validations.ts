fb8bcd384862eb889ce11250dc8e9cd388914b2c
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-10-21 15:36:12 +0900

    Implement Create & Read User Routes

--- lib/validations.ts
+++ lib/validations.ts
@@ -64,4 +64,19 @@ export const AskQuestionSchema = z.object({
     )
     .min(1, { message: "At least one tag is required." })
     .max(3, { message: "You can only add up to 3 tags." })
-})
+});
+
+export const UserSchema = z.object({
+    name: z
+        .string({error: (iss) => iss.input === undefined ? "Name is required" : "Name is invalid"})
+        .min(1, { message: "Name is required" }),
+    username: z
+        .string({error: (iss) => iss.input === undefined ? "Username is required" : "Username is invalid"})
+        .min(3, { message: "Username must be at least 3 characters long." }),
+    email: z.email({ message: "Please provide a valid email address." }),
+    bio: z.string().optional(),
+    image: z.url({ message: "Please provide a valid URL." }).optional(),
+    location: z.string().optional(),
+    portfolio: z.url({ message: "Please provide a valid URL." }).optional(),
+    reputation: z.number().optional(),
+});
