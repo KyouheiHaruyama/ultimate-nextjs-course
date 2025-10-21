5cd8e629c449f7d52e4eec402cb153e7272b2180
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-10-21 18:28:03 +0900

    Implement Create & Read Account Routes

--- lib/validations.ts
+++ lib/validations.ts
@@ -80,3 +80,34 @@ export const UserSchema = z.object({
     portfolio: z.url({ message: "Please provide a valid URL." }).optional(),
     reputation: z.number().optional(),
 });
+
+export const AccountSchema = z.object({
+    userId: z
+        .string({ error: (iss) => iss.input === undefined ? "UserId is required" : "UserId is invalid" })
+        .min(1, { message: "UserId is required." }),
+    name: z
+        .string({ error: (iss) => iss.input === undefined ? "Name is required" : "Name is invalid" })
+        .min(1, { message: "Name is required." }),
+    image: z.url({ message: "Please provide a valid URL." }).optional(),
+    password: z
+        .string()
+        .min(6, { message: "Password must be at least 6 characters long." })
+        .max(100, { message: "Password cannot exceed 100 characters." })
+        .regex(/[A-Z]/, {
+            message: "Password must contain at least one uppercase letter.",
+        })
+        .regex(/[a-z]/, {
+            message: "Password must contain at least one lowercase letter.",
+        })
+        .regex(/[0-9]/, { message: "Password must contain at least one number." })
+        .regex(/[^a-zA-Z0-9]/, {
+            message: "Password must contain at least one special character.",
+        })
+        .optional(),
+    provider: z
+        .string({ error: (iss) => iss.input === undefined ? "Provider is required" : "Provider is invalid" })
+        .min(1, { message: "Provider is required." }),
+    providerAccountId: z
+        .string({ error: (iss) => iss.input === undefined ? "Provider account ID is required" : "Provider account ID is invalid" })
+        .min(1, { message: "Provider account ID is required." }),
+});
