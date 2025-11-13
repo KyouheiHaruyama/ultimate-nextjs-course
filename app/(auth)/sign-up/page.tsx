18efd586e59e8ecdb8d584f2e504bff1d1805242
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-11-13 18:11:52 +0900

    Implement Password/Email Auth

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,13 +3,14 @@
 import React from 'react'
 import AuthForm from "@/components/forms/AuthForm";
 import {SignUpSchema} from "@/lib/validations";
+import {signUpWithCredentials} from "@/lib/actions/auth.actions";
 
 const SignUp = () => {
     return <AuthForm
         formType="SIGN_UP"
         schema={SignUpSchema}
         defaultValues={{ email: "", password: "", name: "", username: "" }}
-        onSubmit={(data) => Promise.resolve({ success: true, data })}
+        onSubmit={signUpWithCredentials}
     />;
 }
 export default SignUp;
