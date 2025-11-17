d32a1eee437a4e17104486dd83adc3f2a1b92141
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-11-17 16:22:13 +0900

    Implement LogOut and Show User Info on Home

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,13 +3,14 @@
 import React from 'react'
 import AuthForm from "@/components/forms/AuthForm";
 import {SignInSchema} from "@/lib/validations";
+import {signInWithCredentials} from "@/lib/actions/auth.actions";
 
 const SignIn = () => {
     return <AuthForm
         formType="SIGN_IN"
         schema={SignInSchema}
         defaultValues={{ email: "", password: "" }}
-        onSubmit={(data) => Promise.resolve({ success: true, data })}
+        onSubmit={signInWithCredentials}
     />;
 }
 export default SignIn;
