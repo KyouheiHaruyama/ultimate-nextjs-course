5e64e5bfb4e9b5162acd7c434ea18b87dffc5d88
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-11-25 11:50:19 +0900

    Implement Edit and Get Question Server Actions and Implemention

--- lib/validations.ts
+++ lib/validations.ts
@@ -121,4 +121,12 @@ export const SignInWithOAuthSchema = z.object({
         email: z.email({ message: "Please provide a valid email address." }),
         image: z.url({ message: "Invalid image URL" }).optional(),
     })
-})
+});
+
+export const EditQuestionSchema = AskQuestionSchema.extend({
+    questionId: z.string().min(1, { message: "Question ID is required." })
+});
+
+export const GetQuestionSchema = z.object({
+    questionId: z.string().min(1, { message: "Question ID is required." })
+});
