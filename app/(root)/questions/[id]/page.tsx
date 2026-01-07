71c3bfa89e437ede92367b0e6ee9516c8cf8fd4b
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2026-01-07 14:55:51 +0900

    Improving Server Calls and UI Rendering in Next.js

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import UserAvatar from "@/components/UserAvatar";
-import {getQuestion} from "@/lib/actions/question.actions";
+import {getQuestion, incrementViews} from "@/lib/actions/question.actions";
 import {notFound, redirect} from "next/navigation";
 import Link from "next/link";
 import {ROUTES} from "@/constants/routes";
@@ -8,7 +8,6 @@ import Metric from "@/components/Metric";
 import {formatNumber, getTimestamp} from "@/lib/utils";
 import TagCard from "@/components/cards/TagCard";
 import Preview from "@/components/editor/Preview";
-import View from "@/app/(root)/questions/view";
 
 // - ** Initial Page Load: ** When a user visits the question details page, the server renders the page with the current view count. This is because the page is a server component, so it's getting executed right on the server.
 // - ** View Count Increment: ** After the page is loaded, a server action is called to increment the view count in the database. This server action is called from the client side, meaning only after the page has been rendered, dom has been created, and a client call is made through `useEffect`.
@@ -19,14 +18,17 @@ const QuestionDetails = async ({ params }: RouteParams) => {
     const { id } = await params;
     if (!id) return notFound();
 
-    const { success, data: question } = await getQuestion({ questionId: id });
-    if (!success || !question) return redirect('/404')
+    const [_, { success, data: question }] = await Promise.all([
+        await incrementViews({ questionId: id }),
+        await getQuestion({ questionId: id })
+    ])
+
+    if (!success || !question) return redirect('/404');
 
     const { author, createdAt, answers, views, tags, content, title } = question;
 
     return (
         <>
-            <View questionId={id} />
             <div className="flex-start w-full flex-col">
                 <div className="flex w-full flex-col-reverse justify-between">
                     <div className="flex items-center justify-start gap-1">
