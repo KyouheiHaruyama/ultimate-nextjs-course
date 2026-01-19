9fa0b3fb61c0073b7a0a01e430577333927df9ce
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2026-01-19 17:13:36 +0900

    Show User Has Saved Question

--- components/questions/SaveQuestion.tsx
+++ components/questions/SaveQuestion.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import {useSession} from "next-auth/react";
-import {useState} from "react";
+import {use, useState} from "react";
 import {toast} from "sonner";
 import Image from "next/image";
 import {toggleSaveQuestion} from "@/lib/actions/collection.actions";
 
-const SaveQuestion = ({ questionId }: { questionId: string }) => {
+const SaveQuestion = ({
+    questionId,
+    hasSavedQuestionPromise
+}: {
+    questionId: string,
+    hasSavedQuestionPromise: Promise<ActionResponse<{ saved: boolean }>>
+}) => {
     const session = useSession();
     const userId = session?.data?.user?.id;
+
+    const { data } = use(hasSavedQuestionPromise);
+
+    const { saved: hasSaved } = data || {};
 
     const [isLoading, setIsLoading] = useState(false);
 
@@ -33,8 +43,6 @@ const SaveQuestion = ({ questionId }: { questionId: string }) => {
             setIsLoading(false);
         }
     };
-
-    const hasSaved = false;
 
     return (
         <Image
