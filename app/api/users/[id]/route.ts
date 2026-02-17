eb243bf57b037bc38bf46c8d42f87b8b43ddc128
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2026-02-17 18:22:04 +0900

    Fix Vercel Deploy error

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -25,8 +25,8 @@ export async function GET(_:Request, { params }: { params: Promise<{ id: string 
 }
 
 // DELETE /api/users/:id
-export async function DELETE(_:Request, { params }: { params: { id: string } }) {
-    const { id } = params;
+export async function DELETE(_:Request, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     if (!id) throw new NotFoundError("User");
 
     try {
@@ -41,8 +41,8 @@ export async function DELETE(_:Request, { params }: { params: { id: string } }) 
     }
 }
 
-export async function PUT(request: Request, {params}: { params: { id: string } }) {
-    const {id} = params;
+export async function PUT(request: Request, {params}: { params: Promise<{ id: string }> }) {
+    const {id} = await params;
     if (!id) throw new NotFoundError("User");
 
     try {
